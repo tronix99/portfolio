99f8b748a6c93701c85175a737d1e86d234ab3b3
RAJ DEVKAR <rajdevkar.6@gmail.com>
2021-02-23 17:16:38 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/portfolio/tronix99.github.io/gh-pages/' : '',
+}
